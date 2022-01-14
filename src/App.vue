2b4a97171f9fad0f15c6b1bc5f68a479104639a1
nikasgithub45 <n.onezashvili.exceedteam@gmail.com>
2022-01-14 18:45:42 +0400

    implemented new feature

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <v-app id="inspire">
+       <Header />
+       <ProductTable/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import ProductTable from './components/ProductTable.vue'
+
+export default {
+  name: "App",
+  components: {
+     Header,
+     ProductTable
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+.v-tabs-bar {
+  float: right !important;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+<v-container >
+  <v-app-bar color="deep-purple accent-4" dark>
+    <template v-slot:extension>
+      <v-text-field
+        label="Filled"
+        placeholder="Search"
+        filled
+        rounded
+        dense
+      ></v-text-field>
+      <v-tabs>
+        <v-btn
+          color="green"
+          depressed
+          elevation="2"
+          large
+          small
+          x-large
+          class="ml-40"
+          >Create</v-btn
+        >
+        <v-tab>products</v-tab>
+        <v-tab>orders</v-tab>
+        <v-tab>Requests</v-tab>
+        <v-tab>Manage</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
+</v-container>
+</template>
